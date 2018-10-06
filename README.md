1b1f1b4ed573b489390453e4560f890c7b03a794
karaxuna <kaxabernet@gmail.com>
2018-10-06 19:15:22 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Setting github
+1. ...
+
+## Setting heroku
+1. Set secret environment variable which you set on github:
+
+```bash
+GITHUB_HOOK_SECRET=mySecret
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var urlParser = require('url');
+var createWebhookHandler = require('github-webhook-handler');
+var updateTargetRepo = require('./lib/updateTargetRepo');
+var webhookHandler = createWebhookHandler({ path: '/github-webhook', secret: process.env.GITHUB_HOOK_SECRET });
+var port = process.env.PORT || 8080;
+
+http.createServer(function (req, res) {
+    webhookHandler(req, res, function () {
+        res.statusCode = 404;
+        res.end('No such location');
+    });
+}).listen(port, function () {
+    console.log('Server listening on port:', port);
+});
+
+webhookHandler.on('error', function (err) {
+    console.error('Error:', err.message);
+});
+
+webhookHandler.on('push', function (e) {
+    var parsedUrl = urlParser.parse(e.url, true);
+    var targetRepoUrl = parsedUrl.query.targetRepoUrl;
+
+    if (!targetRepoUrl) {
+        throw new Error('Target repo url is mandatory!');
+    }
+
+    var ref = e.payload.ref;
+    var sourceRepoUrl = e.payload.repository.ssh_url;
+
+    updateTargetRepo(sourceRepoUrl, targetRepoUrl, ref).catch(function (err) {
+        console.log('Error syncing remote repo.')
+    });
+});
